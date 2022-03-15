3593884223ec467f4b7002e61939c043a34618ca
l-fernandocosta <fernandocostadev98@gmail.com>
2022-03-14 22:44:53 -0300

    about section

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const nav = document.querySelector('#header nav')
 
-const toggle = document.querySelector('nav .toggle')
+const toggle = document.querySelector('nav .toggle');
 const close = document.querySelector('nav .close');
+const links = document.querySelectorAll('nav ul li a');
 
 toggle.addEventListener('click', function(){
   nav.classList.toggle('show')
-})
+});
 
 close.addEventListener('click', function(){
   nav.classList.toggle('show')
-})
+});
+
+for(const link of links){
+  link.addEventListener('click', function(){
+    nav.classList.remove('show');
+  });
+}
